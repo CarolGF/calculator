0be4e7de301eba6aaac6db411aee3c1fc03b5a06
CarolGF <karola_gf@hotmail.com>
2020-04-23 19:31:34 +0100

    Updates

---
+++ precache-manifest.979972bee89269f58cfe87ba6a9d5a41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8eb19374ffb1d2f31e8928daf6635f",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "412f0c62d4017c54058d",
+    "url": "/calculator/static/css/main.80e2efd0.chunk.css"
+  },
+  {
+    "revision": "6f8f701cad8e0e251baa",
+    "url": "/calculator/static/js/2.da86eadb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.da86eadb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412f0c62d4017c54058d",
+    "url": "/calculator/static/js/main.dbb96a77.chunk.js"
+  },
+  {
+    "revision": "bb6ed1b415f33092ff54",
+    "url": "/calculator/static/js/runtime-main.e68c2147.js"
+  }
+]);
